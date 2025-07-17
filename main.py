8658ece9bad360cf6c10b6265c7cd235423c0f05
Jeevan George Joseph <jeevanjoseph1962@gmail.com>
2025-07-17 14:22:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from utils import get_openai_api_key, get_serper_api_key
 
 # Set up API keys and environment
 openai_api_key = get_openai_api_key()
-os.environ["OPENAI_MODEL_NAME"] = 'gpt-3.5-turbo'  # Changed from gpt-4-turbo
+os.environ["OPENAI_MODEL_NAME"] = 'gpt-3.5-turbo'  
 os.environ["SERPER_API_KEY"] = get_serper_api_key()
 
 # Initialize the tools
